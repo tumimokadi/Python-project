5df93d77f057577ef8208ac2e6b2ef3767548441
tumimokadi <tumimokadi101@gmail.com>
2025-07-04 22:23:05 +0200

    My Python First Project

---
+++ __pycache__/rectangle_calc/calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #Function to calculate the area
+    return length * width
+
+def perimeter(length, width):
+    #Function to calculate the perimeter
+    return 2 * (length + width)
